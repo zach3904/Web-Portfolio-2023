aa92d407cf1fffa891c6fdcb62828b2cd33bc27f
Zach Richards <zach3904@gmail.com>
2023-12-08 16:37:30 -0700

    Added prev next arrows for work items. Fixed side scrolling.

--- app/src/pages/Work.js
+++ app/src/pages/Work.js
@@ -4,13 +4,23 @@ import workContent from '../work-content'
 
 function Work() {
 	return (
-    <ul className={styles.workList}>
-      {workContent.map(workItem => (
-        <li key={workItem.title}>
-          <WorkItem {...workItem} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className={styles.workList}>
+        {workContent.map(workItem => (
+          <li key={workItem.title}>
+            <WorkItem {...workItem} />
+          </li>
+        ))}
+      </ul>
+      <div className={styles.prevNextArrows}>
+        <a href="#prev">
+          <span className={`${styles.prevNextIcons} material-icons`}>keyboard_arrow_up</span>
+        </a>
+        <a href="#next">
+          <span className={`${styles.prevNextIcons} material-icons`}>keyboard_arrow_down</span>
+        </a>
+      </div>
+    </>
 	)
 }
 
