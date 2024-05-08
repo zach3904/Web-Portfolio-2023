ed7e02522b94b1b94d53340d3f1e7be4b0bfb361
Zach Richards <zach3904@gmail.com>
2024-05-07 20:23:52 -0700

    Sync up

--- app/src/pages/Contact.tsx
+++ app/src/pages/Contact.tsx
@@ -1,12 +1,23 @@
+import styles from './Contact.module.css'
+
 function Contact() {
 	return (
 		<div>
 			<p>Reprehenderit reprehenderit et exercitation aute magna ut enim nostrud id. Magna irure veniam duis laborum sunt velit amet. Nisi pariatur aliquip irure ullamco anim deserunt commodo irure officia eiusmod occaecat. Sunt et Lorem aliquip aute nulla tempor duis quis in laboris incididunt cupidatat commodo.</p>
 
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Email" />
-        <textarea placeholder="Message"></textarea>
+      <form className={styles.form}>
+        <div className={styles.inputContainer}>
+          <label>Name</label>
+          <input type="text" />
+        </div>
+        <div className={styles.inputContainer}>
+          <label>Email</label>
+          <input type="text" />
+        </div>
+        <div className={styles.inputContainer}>
+          <label>Message</label>
+          <textarea rows={5}></textarea>
+        </div>
         <button type="submit">Send</button>
       </form>
 		</div>
