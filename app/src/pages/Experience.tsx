7f23be700619e56d770471a07a1f27d86be96303
Zach Richards <zach3904@gmail.com>
2024-07-14 23:52:23 -0700

    More experience styling

--- app/src/pages/Experience.tsx
+++ app/src/pages/Experience.tsx
@@ -1,18 +1,23 @@
 import styles from './Experience.module.css'
 import ExperienceWorkItem from '../components/experience/experience-work-item'
 import { experienceContent, educationContent } from '../data/experience-content'
+import Divider from '../components/divider/divider'
 
 function Experience() {
   return (
     <div className={styles.container}>
+      <h2 className={styles.title}>EXPERIENCE</h2>
       {experienceContent.map(item => (
         <ExperienceWorkItem {...item} />
       ))}
+
+      <Divider width="35%" spacing={32} />
+      
+      <h2 className={styles.title}>EDUCATION</h2>
       {educationContent.map(item => (
-        <div>
-          {item.school}
-          {item.degree}
-          {item.period}
+        <div className={styles.educationItem}>
+          <h5 className={styles.school}>{item.school}</h5>
+          <div>{item.degree} | {item.period}</div>
         </div>
       ))}
     </div>
